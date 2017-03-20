36376d64fe468e8db7210f3324a1b80687b9aaeb
Saim Malik <saim.malik@td.com>
2017-03-20 14:02:50 -0400

    added sass to grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -13,20 +13,33 @@ module.exports = function(grunt) {
         }
       }
     },
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css' : 'public/stylesheets/style.scss'
+        }
+      }
+    },
     watch: {
       grunt: { files: ['gruntfile.js'] },
       jade: {
         files: ['./*.jade', 'public/javascripts/*.js', 'public/stylesheets/*.css'],
         tasks: ['jade']
+      },
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
       }
     }
   });
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   // Default task.
   grunt.registerTask('build', 'Convert Jade templates into html templates', ['jade']);
 
   grunt.registerTask('default','Convert Jade templates into html templates',
-                  ['jade','watch']);
-  grunt.loadNpmTasks('grunt-contrib-watch');
+                  ['jade','sass','watch']);
 };
