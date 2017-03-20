c69bae11515501137781285b5f2da6523f8f2fde
Saim Malik <saim.malik@td.com>
2017-03-20 08:21:41 -0400

    Added top bar for dashboard; separated style.scss into signin.scss and dashboard.scss; minor change in signin css

--- public/javascripts/logout-controller.js
+++ public/javascripts/logout-controller.js
@@ -3,16 +3,16 @@
 	app.controller('LogoutController', function($scope, $window) {
 
 		// Create sign in listener
-		firebaseAuth.onAuthStateChanged(function(user) {
-			if (user) {
-				// User is signed in.
-				log("User signed in");
-			} else {
-				// User is signed out.
-				log("User signed out");
-				goToHomePage();
-			}
-		});
+		// firebaseAuth.onAuthStateChanged(function(user) {
+		// 	if (user) {
+		// 		// User is signed in.
+		// 		log("User signed in");
+		// 	} else {
+		// 		// User is signed out.
+		// 		log("User signed out");
+		// 		goToHomePage();
+		// 	}
+		// });
 
 		$scope.signOut = function() {
 			log("signing out");
